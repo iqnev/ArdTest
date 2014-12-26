2d5a17b5d967491c6e80fd90f2819aae3abd1143
fortran <qnev89@gmail.com>
2014-12-26 20:44:24 +0200

    java rxtx SerialWriter

--- src/testcomunication/SerialCommunication.java
+++ src/testcomunication/SerialCommunication.java
@@ -5,21 +5,82 @@
  */
 package testcomunication;
 
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEventListener;
+import java.io.InputStream;
+import java.io.OutputStream;
+import gnu.io.CommPort;
+
 /**
  *
  * @author iqnev
  */
-public class SerialCommunication {
+public class SerialCommunication{
+    
+    private static SerialCommunication instance = null;
+    private static boolean coonected = false;
+    
+    /**
+     * Milliseconds to block while waiting for port open
+     */
+    private static final int TIME_OUT = 2000;
+    /**
+     * Default bits per second for COM port.
+     */
+    private static final int DATA_RATE = 9600;
     
     private SerialCommunication() {
+        super();
+        try {
+          //  connect("COM4");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        SerialCommunication.coonected = true;
     }
     
     public static SerialCommunication getInstance() {
-        return SerialCommunicationHolder.INSTANCE;
+        if(instance == null) {
+            instance = new SerialCommunication();          
+        }
+        
+         return instance;
     }
     
-    private static class SerialCommunicationHolder {
+    void connect(String portName) throws Exception {
+        CommPortIdentifier portIdentifier = CommPortIdentifier
+                .getPortIdentifier(portName);
+        if (portIdentifier.isCurrentlyOwned()) {
+            System.out.println("Error: Port is currently in use");
+        } else {
+            
+            //TODO last params to be global 
+            CommPort commPort = portIdentifier.open(this.getClass().getName(),
+                    TIME_OUT);
 
-        private static final SerialCommunication INSTANCE = new SerialCommunication();
+            if (commPort instanceof SerialPort) {
+                SerialPort serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(DATA_RATE, 
+                        SerialPort.DATABITS_8,
+                        SerialPort.STOPBITS_1, 
+                        SerialPort.PARITY_NONE);
+                
+                // open the streams
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+
+            //    (new Thread(new SerialReader(in))).start();
+              //  (new Thread(new SerialWriter(out))).start();
+
+            } else {
+                System.out
+                        .println("Error: Only serial ports are handled by this example.");
+            }
+        }
     }
+    
+    
+  
 }
