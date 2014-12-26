807156c6ced5249bc54f71a831d9ae14610ef61b
fortran <qnev89@gmail.com>
2014-12-26 21:13:07 +0200

    reader and writer class

--- src/testcomunication/SerialWriter.java
+++ src/testcomunication/SerialWriter.java
@@ -9,14 +9,6 @@ package testcomunication;
  *
  * @author iqnev
  */
-public class TestComunication {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class SerialWriter {
     
 }
-
